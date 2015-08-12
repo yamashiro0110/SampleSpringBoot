32f42fcd6229a575df5df0e5a91afe6489f9ca02
yamashiro0110 <yamashiro0110@gmail.com>
2015-08-13 00:52:53 +0900

    アプリケーション起動時に実行されるDML

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    user (name, mail, tel, age)
+VALUES
+    ('yamashiro', 'yamashiro@spring.io', '000-111-2222', 18),
+    ('miyamoto', 'test1@spring.io', '000-111-2222', 19),
+    ('yokota', 'test2@spring.io', '000-111-2222', 20)
+;
