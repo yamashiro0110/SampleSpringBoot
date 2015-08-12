ba2fb7d29e8ff4a6f55dbea90165faebd6b3495b
yamashiro0110 <yamashiro0110@gmail.com>
2015-08-13 00:49:24 +0900

    UserのメールアドレスをUniqueに変更

--- src/main/java/sample/user/domain/User.java
+++ src/main/java/sample/user/domain/User.java
@@ -4,21 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Email;
-import org.springframework.format.annotation.NumberFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
-/**
- * userテーブルのエンティティ
- */
 @Entity
 @Data
 @NoArgsConstructor
@@ -35,6 +28,7 @@ public class User implements Serializable {
     @NotNull(message = "メールアドレスが未入力です")
     @Size(min = 1, max = 100, message = "メールアドレスを入力してください")
     @Email(message = "不正なメールアドレスです")
+    @Column(unique = true)
     private String mail;
 
     @NotNull(message = "電話番号が未入力です")
