cb44314e172510df69b74ad835e0d42fe3f124c4
yamashiro0110 <yamashiro0110@gmail.com>
2015-08-16 19:56:52 +0900

    #3 テーブル構成変更のため

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,17 @@
 INSERT INTO
-    user (name, mail, tel, age)
+    user (id, name, tel, age)
 VALUES
-    ('yamashiro', 'yamashiro@spring.io', '000-111-2222', 18),
-    ('miyamoto', 'test1@spring.io', '000-111-2222', 19),
-    ('yokota', 'test2@spring.io', '000-111-2222', 20)
+    (1, 'yamashiro', '000-111-2222', 18),
+    (2, 'miyamoto', '000-111-2222', 19),
+    (3, 'yokota', '000-111-2222', 20)
 ;
+
+INSERT INTO
+    auth_user (id, mail, hashed_password)
+VALUES
+    (1, 'yamashiro@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    (2, 'test1@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    (3, 'test2@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.')
+;
+
+
