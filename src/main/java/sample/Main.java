4ac3f23d0050dc8b422b62ab67e4a1cd5da4d585
yamashiro0110 <yamashiro0110@gmail.com>
2015-08-16 20:03:31 +0900

    エントリーポイントのクラスを修正

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,25 +1,13 @@
-package sample.user; /**
+package sample; /**
  * Created by yamashiro-r on 15/07/28.
  */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * アプリケーション起動クラス
- */
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
-        testMvc(args);
-    }
-
-    /**
-     * MVCアプリケーションを起動する
-     * @param args
-     */
-    private static void testMvc(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 }
