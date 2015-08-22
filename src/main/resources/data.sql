01466dc4383f72fb873a3ff543092aa5b17d394e
yamashiro0110 <yamashiro0110@gmail.com>
2015-08-22 21:15:32 +0900

    データ追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,53 @@
 INSERT INTO
-    user (id, name, tel, age)
+    user (name, tel, age)
 VALUES
-    (1, 'yamashiro', '000-111-2222', 18),
-    (2, 'miyamoto', '000-111-2222', 19),
-    (3, 'yokota', '000-111-2222', 20)
+    ('yamashiro', '000-111-2222', 18),
+    ('miyamoto', '000-111-2222', 19),
+    ('yokota', '000-111-2222', 20),
+    ('aaa', '000-111-2222', 21),
+    ('bbb', '000-111-2222', 21),
+    ('ccc', '000-111-2222', 21),
+    ('ddd', '000-111-2222', 21),
+    ('eee', '000-111-2222', 21),
+    ('fff', '000-111-2222', 21),
+    ('ggg', '000-111-2222', 21),
+    ('hhh', '000-111-2222', 21),
+    ('iii', '000-111-2222', 21),
+    ('jjj', '000-111-2222', 21)
 ;
 
 INSERT INTO
-    auth_user (id, mail, hashed_password)
+    auth_user (mail, hashed_password)
 VALUES
-    (1, 'yamashiro@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
-    (2, 'test1@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
-    (3, 'test2@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.')
+    ('yamashiro@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test1@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test2@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test3@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test4@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test5@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test6@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test7@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test8@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test9@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test10@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test11@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.'),
+    ('test12@spring.io', '$2a$04$wtheo17dIX8jEEQ2eHtz7Oxz89IbIMaThjGIEva/jbQcoSX1Dxys.')
 ;
 
-
+INSERT INTO
+    address (postal_code, prefectures, city, address, building)
+VALUES
+    ('1111111', '沖縄県', '那覇市', 'おもろまち１−２−３', 'ｘｘｘマンション'),
+    ('1111112', '東京都', '中央区', '日本橋１−２−３', 'ｘｘｘビル'),
+    ('1111113', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111114', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111115', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111116', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111117', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111118', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111119', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111120', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111121', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111122', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート'),
+    ('1111123', '沖縄', '那覇市', '久茂地１−２−３', 'ｘｘｘアパート')
+;
