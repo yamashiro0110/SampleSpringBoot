9aee8fa738e8760dc3390d9b18bf305f575db2d0
yamashiro0110 <yamashiro0110@gmail.com>
2015-08-26 01:30:40 +0900

    Userをnameで検索するAPI

--- src/main/java/sample/user/repository/UserRepository.java
+++ src/main/java/sample/user/repository/UserRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    public List<User> findByNameStartingWith(String name);
 }
