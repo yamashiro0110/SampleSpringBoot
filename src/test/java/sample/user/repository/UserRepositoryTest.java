d77c0bfb69429764f2092f808b4809c3d7d17621
yamashiro0110 <yamashiro0110@gmail.com>
2015-09-05 12:53:35 +0900

    テスト修正

--- src/test/java/sample/user/repository/UserRepositoryTest.java
+++ src/test/java/sample/user/repository/UserRepositoryTest.java
@@ -8,17 +8,18 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.domain.Page;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import sample.Main;
-import sample.user.address.domain.Address;
-import sample.user.domain.User;
-import sample.user.service.UserProtoTypeService;
+import sample.domain.user.address.Address;
+import sample.domain.user.User;
+import sample.service.user.UserFindService;
 
 import javax.annotation.Resource;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Main.class)
 public class UserRepositoryTest extends TestCase {
+
     @Resource
-    private UserProtoTypeService userProtoTypeService;
+    private UserFindService userFindService;
 
     @Test
     public void findByAddress() {
@@ -33,7 +34,7 @@ public class UserRepositoryTest extends TestCase {
 
         Boolean.valueOf(true);
 
-        Page<User> users = userProtoTypeService.findByAddress(address);
+        Page<User> users = userFindService.findByAddress(address);
         users.forEach(user -> System.out.println("user:" + ReflectionToStringBuilder.toString(user)));
     }
 }
