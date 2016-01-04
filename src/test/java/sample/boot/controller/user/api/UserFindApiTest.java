de08fecbc683b8da3d4de5f13de6c03ec15135c7
yamashiro.ryota <yamashiro.ryota@actier.co.jp>
2016-01-04 23:29:19 +0900

    add controller test

--- src/test/java/sample/boot/controller/user/api/UserFindApiTest.java
+++ src/test/java/sample/boot/controller/user/api/UserFindApiTest.java
@@ -4,39 +4,47 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import sample.boot.Main;
+import sample.boot.domain.user.User;
+import sample.boot.repository.user.UserRepository;
 
 import javax.annotation.Resource;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
 @WebAppConfiguration
-//@ContextConfiguration(classes = Main.class)
-@SpringApplicationConfiguration(classes = {JacksonAutoConfiguration.class})
 public class UserFindApiTest {
     private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Resource
+    private UserRepository userRepository;
     @Resource
     private ObjectMapper objectMapper;
-    @InjectMocks
-    private UserFindApi userFindApi;
 
     @Before
     public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(userFindApi).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
     public void testUser() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/find/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        User user = userRepository.findOne(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/find/{id}", Long.valueOf(1)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().string(objectMapper.writeValueAsString(user)));
     }
 }
