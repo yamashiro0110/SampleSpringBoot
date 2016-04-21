40413bd537b0eec3869c1ce4454e3ff307d83abe
r_yamashiro <yamashiro.ryota@actier.co.jp>
2016-04-21 09:25:49 +0900

    test jackson

--- src/main/java/sample/boot/api/HogeController.java
+++ src/main/java/sample/boot/api/HogeController.java
@@ -1,20 +1,69 @@
 package sample.boot.api;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 @RequestMapping("/api/hoge")
 public class HogeController {
 
-    @RequestMapping(method = RequestMethod.POST)
-    public Map<String, String> result() {
-        Map<String, String> result = new HashMap<>();
-        result.put("status", "ok");
-        return result;
+    @RequestMapping(value = "hoge", method = GET)
+    public Hoge hoge() {
+        return new Hoge();
     }
+
+    @RequestMapping(value = "fuga", method = GET)
+    public Fuga fuga() {
+        return new Fuga();
+    }
+
+    @RequestMapping(value = "hogefuga", method = GET)
+    public HogeFuga hogeFuga() {
+        return new HogeFuga();
+    }
+
+    @RequestMapping(value = "hogefuga/list", method = GET)
+    public HogeFugaList hogeFugaList() {
+        return new HogeFugaList();
+    }
+
+    @Data
+    public static class Hoge {
+        private String name = "hoge";
+        private List<String> list = Arrays.asList("hoge", "hoge", "hoge");
+    }
+
+    @Data
+    public static class Fuga {
+        private String name = "fuga";
+        private HashMap map = new HashMap();
+
+        {
+            map.put("key1", "fuga");
+            map.put("key2", "Fuga");
+        }
+    }
+
+    @Data
+    public static class HogeFuga {
+        private String name = "hogefuga";
+        private Hoge hoge = new Hoge();
+        private Fuga fuga = new Fuga();
+    }
+
+    @Data
+    public static class HogeFugaList {
+        private String name = "";
+        private List<String> hoge = Collections.emptyList();
+        private Fuga fuga = new Fuga();
+    }
+
 }
--- src/main/java/sample/boot/config/WebConfig.java
+++ src/main/java/sample/boot/config/WebConfig.java
@@ -1,27 +1,23 @@
 package sample.boot.config;
 
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import sample.boot.exception.TestErrorHandleException;
 
-import javax.servlet.*;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class WebConfig {
 
-    @Bean
+    //    @Bean
     public FilterRegistrationBean filterRegistrationBean() {
         FilterRegistrationBean bean = new FilterRegistrationBean();
         bean.addUrlPatterns("/api/hoge");
-        bean.setFilter(new TestFilter());
+//        bean.setFilter(new TestFilter());
         return bean;
     }
 
@@ -34,12 +30,12 @@ public class WebConfig {
         return result;
     }
 
-    private class TestFilter extends BasicAuthenticationFilter {
-
-        @Override
-        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-            throw new TestErrorHandleException("exception from test filter");
-        }
-    }
+//    private class TestFilter extends BasicAuthenticationFilter {
+//
+//        @Override
+//        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+////            throw new TestErrorHandleException("exception from test filter");
+//        }
+//    }
 
 }
