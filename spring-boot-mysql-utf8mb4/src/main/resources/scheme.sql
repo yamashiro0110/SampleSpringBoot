9d83a0fc341f3da97dcb399dad3f5d98fc57abfd
yamashiro.ryota <yamashiro0110@gmail.com>
2017-02-13 23:34:52 +0900

    change sql

--- spring-boot-mysql-utf8mb4/src/main/resources/scheme.sql
+++ spring-boot-mysql-utf8mb4/src/main/resources/scheme.sql
@@ -1,7 +1,16 @@
-drop table if exists sample_table;
+DROP TABLE if EXISTS sample_table;
 
-create table sample_table(
-    sample_id INT NOT NULL PRIMARY KEY,
+CREATE TABLE sample_table(
+    sample_id INT NOT NULL,
     sample_name VARCHAR(50) NOT NULL,
-    created DATETIME NOT NULL
+    created DATETIME NOT NULL,
+    PRIMARY KEY(sample_id)
+);
+
+DROP TABLE if EXISTS sample_text_table;
+
+CREATE TABLE sample_text_table (
+    sample_id INT NOT NULL,
+    sample_text TEXT NOT NULL,
+    PRIMARY KEY(sample_id)
 );
