446f459bfcabf65cad10892960f00258e896a6de
yamashiro.ryota <yamashiro0110@gmail.com>
2017-02-13 23:34:40 +0900

    add spring-boot-kotlin

---
+++ spring-boot-kotlin/src/main/kotlin/sample/spring/boot/SpringBootKotlinApplication.kt
@@ -0,0 +1,32 @@
+package sample.spring.boot
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import javax.annotation.Resource
+
+@SpringBootApplication
+open class SpringBootKotlinApplication : CommandLineRunner {
+
+    @Resource(name = "sample_message")
+    private var message = ""
+
+    override fun run(vararg args: String?) {
+        println("run kotlin")
+        println("message is $message")
+    }
+
+    @Bean("sample_message")
+    @ConditionalOnProperty(prefix = "sample.kotlin.msg", name = arrayOf("enable"))
+    open fun messageIfTrue() = "hello kotlin!!"
+
+    @Bean("sample_message")
+    @ConditionalOnProperty(prefix = "sample.kotlin.msg", name = arrayOf("disable"))
+    open fun messageIfFalse() = "bye kotlin..."
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringBootKotlinApplication::class.java, *args)
+}
