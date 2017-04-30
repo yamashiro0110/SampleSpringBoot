bb56357cf72ce9eee67b9916690c4d5ae8dbf420
yamashiro.ryota <yamashiro0110@gmail.com>
2017-04-30 13:39:27 +0900

    ログ出力

--- spring-boot-encoding-filter/src/main/java/com/example/controller/ShiftJisController.java
+++ spring-boot-encoding-filter/src/main/java/com/example/controller/ShiftJisController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
 import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,14 +21,17 @@ import java.nio.file.Paths;
 @RestController
 @RequestMapping("/")
 public class ShiftJisController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShiftJisController.class);
 
     @PostMapping
     String post(@RequestBody String text) throws IOException {
+        LOGGER.debug("sjis:{}", text);
+
         Charset sjis = Charset.forName("SJIS");
         Charset utf8 = StandardCharsets.UTF_8;
 
         String encodeText = new String(text.getBytes(sjis), utf8);
-        System.out.println(encodeText);
+        LOGGER.debug("utf8:{}", encodeText);
 
         Path path = Paths.get("tmp", "text.log").toAbsolutePath();
 
--- spring-boot-encoding-filter/src/test/java/com/example/controller/SleepControllerTest.java
+++ spring-boot-encoding-filter/src/test/java/com/example/controller/SleepControllerTest.java
@@ -6,7 +6,8 @@ import okhttp3.Response;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.SocketTimeoutException;
@@ -16,13 +17,14 @@ import java.util.concurrent.TimeUnit;
  * Created by yamashiro-r on 2017/04/30.
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 public class SleepControllerTest {
 
     @Test(expected = SocketTimeoutException.class)
     public void sleep() throws Exception {
         OkHttpClient httpClient = new OkHttpClient.Builder()
-                .connectTimeout(10, TimeUnit.SECONDS)
+                .connectTimeout(3, TimeUnit.SECONDS)
                 .build();
 
         Request request = new Request.Builder()
