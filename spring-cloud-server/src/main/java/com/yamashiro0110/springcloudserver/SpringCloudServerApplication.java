d4e09d9c475e6a60a992042a374b9534e9f42e8b
yamashiro.ryota <yamashiro0110@gmail.com>
2017-08-26 12:20:14 +0900

    add spring cloud server

---
+++ spring-cloud-server/src/main/java/com/yamashiro0110/springcloudserver/SpringCloudServerApplication.java
@@ -0,0 +1,14 @@
+package com.yamashiro0110.springcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServerApplication.class, args);
+    }
+}
